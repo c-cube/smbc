91d613034e5e54ae772054c0299c5c5220f21fba
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-07-06 14:46:12 +0200

    main: interface with Solver

--- src/e3.ml
+++ src/e3.ml
@@ -1,8 +1,6 @@
 
 (* This file is free software. See file "license" for more details. *)
 
-open! Solver (* TODO: really use it *)
-
 let parse_file file =
   let res = CCSexpM.parse_file_list file in
   match res with
@@ -12,9 +10,23 @@ let parse_file file =
         | Result.Error msg -> print_endline msg; exit 1
         | Result.Ok ast -> ast
 
+let solve (ast:Ast.statement list) : unit =
+  let module S = Solver.Make(struct end) in
+  (* add statements *)
+  S.add_statement_l ast;
+  (* solve *)
+  match S.check () with
+    | S.Sat _ ->
+      Format.printf "result: @{<Green>sat@}@."
+    | S.Unsat ->
+      Format.printf "result: @{<Yellow>unsat@}@."
+    | S.Unknown _ ->
+      Format.printf "result: @{<blue>unknown@}@."
+
 (** {2 Main} *)
 
 let print_input_ = ref false
+let color_ = ref true
 let file = ref ""
 let set_file s =
   if !file = "" then file := s
@@ -22,15 +34,19 @@ let set_file s =
 
 let options =
   Arg.align [
-    "--print-input", Arg.Set print_input_, " print input"
+    "--print-input", Arg.Set print_input_, " print input";
+    "-nc", Arg.Clear color_, " do not use colors"
   ]
 
 let () =
   Arg.parse options set_file "experimental SMT solver";
   if !file = "" then failwith "provide one file";
+  if !color_ then CCFormat.set_color_tag_handling CCFormat.stdout;
+  (* parse *)
   let ast = parse_file !file in
   if !print_input_
   then 
     Format.printf "@[parsed:@ @[<v>%a@]@]@."
       (CCFormat.list ~start:"" ~stop:"" ~sep:"" Ast.pp_statement) ast;
-  failwith "todo"
+  (* solve *)
+  solve ast
