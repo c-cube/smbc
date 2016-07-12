9c270ebbfdee9919e5821cf2196d048fa3fb6fbb
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-07-12 17:46:52 +0200

    larger timeout for tests

---
+++ examples/map_fun3.lisp
@@ -0,0 +1,27 @@
+
+; find `f` where: `map (f plus) [1,2,3] = [2,4,6], f 10=20`,
+; same as map_fun2, but easier
+
+; expect: SAT
+
+(include "nat.lisp")
+(include "list.lisp")
+
+(define (1 nat (s z)))
+(define (2 nat (s 1)))
+(define (3 nat (s 2)))
+(define (4 nat (s 3)))
+(define (5 nat (s 4)))
+(define (6 nat (s 5)))
+(define (10 nat (mult 2 5)))
+
+(goal
+  ((f (-> (-> nat nat nat) nat nat)))
+  (and
+    (=
+      (map (f plus) (cons 1 (cons 2 (cons 3 nil))))
+      (cons 2 (cons 4 (cons 6 nil))))
+    (= (f plus 10) (mult 2 10))
+  ))
+
+
