ac3937379e8cdfcca3362b5548c7ca2968dd572e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-12-02 15:34:04 +0100

    another gc tuning
    now that memleak in msat is fixed

--- src/smbc.ml
+++ src/smbc.ml
@@ -122,7 +122,7 @@ let setup_timeout_ t =
 
 let setup_gc () =
   let g = Gc.get () in
-  g.Gc.space_overhead <- 300; (* major gc *)
+  g.Gc.space_overhead <- 3_000; (* major gc *)
   g.Gc.max_overhead <- 10_000; (* compaction *)
   g.Gc.minor_heap_size <- 500_000; (* ×8 to obtain bytes on 64 bits -->  *)
   Gc.set g
